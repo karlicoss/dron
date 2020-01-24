a2845504509ee2d49f4f46e5f7b6e2a4507ec78d
Dima Gerasimov <karlicoss@gmail.com>
2020-01-24 23:44:20 +0000

    prettify a bit

--- systemdtab.py
+++ systemdtab.py
@@ -1,62 +1,35 @@
 #!/usr/bin/env python3
+import argparse
 import getpass
-from pathlib import Path
 import os
+from pathlib import Path
 import shutil
 from subprocess import check_call, CalledProcessError, run, PIPE, check_output
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 from typing import NamedTuple, Union, Sequence, Optional, Iterator
 
 
-# TODO timer spec?
-class Timer(NamedTuple):
-    pass
+DIR = Path("~/.config/systemd/user").expanduser()
+# TODO FIXME mkdir in case it doesn't exist..
 
-# TODO use python's literate types?
-# TODO
-#        The following special expressions may be used as shorthands for longer normalized forms:
-#
-#                minutely → *-*-* *:*:00
-#                  hourly → *-*-* *:00:00
-#                   daily → *-*-* 00:00:00
-#                 monthly → *-*-01 00:00:00
-#                  weekly → Mon *-*-* 00:00:00
-#                  yearly → *-01-01 00:00:00
-#               quarterly → *-01,04,07,10-01 00:00:00
-#            semiannually → *-01,07-01 00:00:00
-
-
-# TODO wow, that's quite annoying. so timer has to be separate file. oh well.
-
-
-# TODO tui for confirming changes, show short diff?
-
-# TODO actually for me, stuff like 'hourly' makes little sense; I usually space out in time..
-When = str
-
-
-MANAGED_MARKER = 'Systemdtab=true'
-
-def is_managed(body: str):
-    return MANAGED_MARKER in body
-
-
-# TODO how to come up with good implicit job name?
-def timer(*, unit_name: str, when: When) -> str:
-    return f'''
-# managed by systemdtab
-[Unit]
-Description=Timer for {unit_name}
-{MANAGED_MARKER}
-
-[Timer]
-OnCalendar={when}
-'''
 
 PathIsh = Union[str, Path]
 
-Command = Union[PathIsh, Sequence[PathIsh]]
 
-from tempfile import NamedTemporaryFile, TemporaryDirectory
+# def scu(*args, method=check_call, **kwargs):
+#     return method(['systemctl', '--user', *args], **kwargs) # TODO status???
+
+def scu(*args):
+    return ['systemctl', '--user', *args]
+
+
+def reload():
+    check_call(scu('daemon-reload'))
+
+
+MANAGED_MARKER = 'Systemdtab=true'
+def is_managed(body: str):
+    return MANAGED_MARKER in body
 
 
 def test_managed():
@@ -70,10 +43,43 @@ ExecStart=echo 123
     assert not is_managed(custom)
 
 
+When = str
+# TODO how to come up with good implicit job name?
+def timer(*, unit_name: str, when: When) -> str:
+    return f'''
+# managed by systemdtab
+[Unit]
+Description=Timer for {unit_name}
+{MANAGED_MARKER}
+
+[Timer]
+OnCalendar={when}
+'''
+
+
+Command = Union[PathIsh, Sequence[PathIsh]]
+# TODO allow to pass extra args
+def service(*, unit_name: str, command: Command) -> str:
+    # TODO FIXME think carefully about escaping command etc?
+    res = f'''
+# managed by systemdtab
+[Unit]
+Description=Service for {unit_name}
+OnFailure=status-email@%n.service
+
+[Service]
+ExecStart=bash -c "{command}"
+# StandardOutput=file:/L/tmp/alala.log
+{MANAGED_MARKER}
+'''
+    # TODO not sure if should include username??
+    return res
+
+
 def verify(*, unit_file: str, contents: str):
-    # TODO ugh pipe doesn't work??
-# systemd-analyze --user verify <(cat systemdtab-test.service)       I  18:58:02  
-# Failed to prepare filename /proc/self/fd/11: Invalid argument
+    # ugh. pipe doesn't work??
+    # systemd-analyze --user verify <(cat systemdtab-test.service)
+    # Failed to prepare filename /proc/self/fd/11: Invalid argument
     with TemporaryDirectory() as tdir:
         sfile = Path(tdir) / unit_file
         sfile.write_text(contents)
@@ -101,7 +107,7 @@ def test_verify():
 ExecStart=echo 123
 ''')
 
-    ok(contents=unit(unit_name='alala', command='echo 123'))
+    ok(contents=service(unit_name='alala', command='echo 123'))
 
     # garbage
     fails(contents='fewfewf')
@@ -117,54 +123,6 @@ StandardOutput=journal
 ExecStart=yes
 StandardOutput=baaad
 ''')
-
-
-def unit(*, unit_name: str, command: Command) -> str:
-    # TODO allow to pass extra args
-    # TODO FIXME think carefully about escaping etc?
-    res = f'''
-# managed by systemdtab
-# TODO description unnecessary?
-[Service]
-ExecStart=bash -c "{command}"
-# StandardOutput=file:/L/tmp/alala.log
-{MANAGED_MARKER}
-
-[Unit]
-OnFailure=status-email@%n.service
-Requires=systemdtab.target
-
-'''
-    # TODO not sure if should include username??
-    return res
-# TODO need to install systemdtab-email thing?
-# TODO dunno, separate script might be nicer to test?
-
-    # TODO FIXME
-
-# https://bugs.python.org/issue31528 eh, probably can't use configparser.. plaintext is good enough though.
-
-
-# TODO later, implement logic for cleaning up old jobs
-
-
-# TODO FIXME mkdir in case it doesn't exist..
-
-
-DIR = Path("~/.config/systemd/user").expanduser()
-
-# TODO not sure if should do one by one or all at once?
-# yeah, makes sense to do all at once...
-# TODO warn about dirty state?
-
-
-
-def scu(*args, method=check_call, **kwargs):
-    return method(['systemctl', '--user', *args], **kwargs) # TODO status???
-
-
-def sscu(*args):
-    return ['systemctl', '--user', *args]
 
 
 def write_unit(*, unit_file: str, contents: str) -> None:
@@ -199,12 +157,12 @@ ExecStart={target} {user} %i
 '''
     # TODO copy the file to local??
     write_unit(unit_file=f'status-email@.service', contents=X)
-    # I guess makes sense to reaload here; fairly atomic step
-    scu('daemon-reload')
+    # I guess makes sense to reload here; fairly atomic step
+    reload()
 
 
 def finalize():
-    scu('daemon-reload')
+    reload()
 
 
 # TODO think about arg names?
@@ -219,13 +177,13 @@ def job(when: Optional[When], command: Command, *, unit_name: Optional[str]=None
     # TODO generate unit name
     # TODO not sure about names.
     # I guess warn user about non-unique names and prompt to give a more specific name?
-    u = unit(unit_name=unit_name, command=command)
-    write_unit(unit_file=unit_name + '.service', contents=u)
+    s = service(unit_name=unit_name, command=command)
+    write_unit(unit_file=unit_name + '.service', contents=s)
 
     if when is not None:
         t = timer(unit_name=unit_name, when=when)
         write_unit(unit_file=unit_name + '.timer', contents=t)
-        scu('start', unit_name + '.timer')
+        check_call(scu('start', unit_name + '.timer'))
     # TODO otherwise just unit status or something?
 
     # TODO FIXME enable?
@@ -233,19 +191,15 @@ def job(when: Optional[When], command: Command, *, unit_name: Optional[str]=None
     # TODO copy files with rollback? not sure how easy it is..
 
 
-# TODO test with 'fake' systemd dir?
 
-import argparse
-
-
-def managed() -> Iterator[str]:
-    res = scu('list-unit-files', '--no-pager', '--no-legend', method=check_output).decode('utf8')
+def managed_units() -> Iterator[str]:
+    res = check_output(scu('list-unit-files', '--no-pager', '--no-legend')).decode('utf8')
     units = [x.split()[0] for x in res.splitlines()]
     for u in units:
         # meh. but couldn't find any better way to filter a subset of systemd properties...
         # e.g. sc show only displays 'known' properties.
         # could filter by description? but bit too restrictive?
-        res = scu('cat', u, method=check_output).decode('utf8')
+        res = check_output(scu('cat', u)).decode('utf8')
         if is_managed(res):
             yield res
 
@@ -263,13 +217,10 @@ def main():
     mode = args.mode; assert mode is not None
 
     if mode == 'managed':
-        for u in managed():
+        for u in managed_units():
             print(u)
     elif mode == 'timers':
-        os.execvp('watch', ['watch', '-n', '0.5', ' '.join(sscu('list-timers'))])
-
-
-
+        os.execvp('watch', ['watch', '-n', '0.5', ' '.join(scu('list-timers'))])
 
 
 if __name__ == '__main__':
@@ -292,3 +243,41 @@ if __name__ == '__main__':
 
 # TODO wonder if I remove timers, do they drop counts?
 # TODO FIXME ok, for now, it's fine, but with more sophisticated timers might be a bit annoying
+
+# TODO use python's literate types?
+# TODO
+#        The following special expressions may be used as shorthands for longer normalized forms:
+#
+#                minutely → *-*-* *:*:00
+#                  hourly → *-*-* *:00:00
+#                   daily → *-*-* 00:00:00
+#                 monthly → *-*-01 00:00:00
+#                  weekly → Mon *-*-* 00:00:00
+#                  yearly → *-01-01 00:00:00
+#               quarterly → *-01,04,07,10-01 00:00:00
+#            semiannually → *-01,07-01 00:00:00
+
+
+# TODO wow, that's quite annoying. so timer has to be separate file. oh well.
+
+
+# TODO tui for confirming changes, show short diff?
+
+# TODO actually for me, stuff like 'hourly' makes little sense; I usually space out in time..
+
+# TODO need to install systemdtab-email thing?
+# TODO dunno, separate script might be nicer to test?
+
+
+# https://bugs.python.org/issue31528 eh, probably can't use configparser.. plaintext is good enough though.
+
+
+# TODO later, implement logic for cleaning up old jobs
+
+
+# TODO not sure if should do one by one or all at once?
+# yeah, makes sense to do all at once...
+# TODO warn about dirty state?
+
+
+# TODO test with 'fake' systemd dir?
