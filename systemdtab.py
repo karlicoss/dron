f806e05296d4a61bb5ac6dbdd869cb78c6f24c1a
Dima Gerasimov <karlicoss@gmail.com>
2020-01-24 19:00:26 +0000

    more work, copy to systemd dir; start verifying

--- systemdtab.py
+++ systemdtab.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from typing import NamedTuple, Union
+from pathlib import Path
+from subprocess import check_call
+from typing import NamedTuple, Union, Sequence, Optional
 
 
 # TODO timer spec?
@@ -25,41 +27,95 @@ class Timer(NamedTuple):
 
 # TODO tui for confirming changes, show short diff?
 
+# TODO actually for me, stuff like 'hourly' makes little sense; I usually space out in time..
 When = str
 
 # TODO how to come up with good implicit job name?
-def timer(name: str, when: When) -> str:
+def timer(*, unit_name: str, when: When) -> str:
     return f'''
 # managed by systemdtab
 [Unit]
-Description=Timer for {name}
+Description=Timer for {unit_name}
 
 [Timer]
 OnCalendar={when}
 '''
+
+PathIsh = Union[str, Path]
+
+Command = Union[PathIsh, Sequence[PathIsh]]
+
+from tempfile import NamedTemporaryFile
+def verify(contents: str):
+    # TODO ugh pipe doesn't work??
+# systemd-analyze --user verify <(cat systemdtab-test.service)       I  18:58:02  
+# Failed to prepare filename /proc/self/fd/11: Invalid argument
+    with NamedTemporaryFile() as f:
+        Path(f.name).write_text(contents)
+        check_call(['systemd-analyze', 'verify', f.name])
+
+
+def unit(*, unit_name: str, command: Command) -> str:
+    # TODO allow to pass extra args
+    res = f'''
+# managed by systemdtab
+'''
+    verify(res)
+    return res
+
+    # TODO FIXME
 
 # https://bugs.python.org/issue31528 eh, probably can't use configparser.. plaintext is good enough though.
 
 
 # TODO later, implement logic for cleaning up old jobs
 
-from pathlib import Path
-PathIsh = Union[str, Path]
 
-Command = Union[PathIsh, List[PathIsh]]
+# TODO FIXME mkdir in case it doesn't exist..
+
+
+DIR = Path("~/.config/systemd/user").expanduser()
+
+# TODO not sure if should do one by one or all at once?
+# yeah, makes sense to do all at once...
+# TODO warn about dirty state?
+
+
+
+def scu(*args, **kwargs):
+    check_call(['systemctl', '--user', *args], **kwargs) # TODO status???
+
 
 # TODO think about arg names?
 # TODO not sure if should give it default often?
 # TODO when first? so it's more compat to crontab..
-def job(cmd: Command, *, when: When):
-    pass
+def job(when: When, command: Command, *, unit_name: Optional[str]=None):
+    assert unit_name is not None
+    # TODO generate unit name
+    # TODO not sure about names.
+    # I guess warn user about non-unique names and prompt to give a more specific name?
+    u = unit(unit_name=unit_name, command=command)
+    t = timer(unit_name=unit_name, when=when)
+    # TODO would be nice to revert....
+    # TODO assert that managed by systemdtab
+    # TODO name it systemdsl?
+    # TODO not sure what rollback should do w.r.t to
+    # TODO perhaps, only reenable changed ones? ugh. makes it trickier...
+    (DIR / unit_name).with_suffix('.service').write_text(u)
+    (DIR / unit_name).with_suffix('.timer').write_text(t)
+    # TODO FIXME enable?
+    scu('start', unit_name)
+
+
+
+
 
 def test():
     # TODO 'fake' systemd dir?
-    job(
-        cmd=, # TODO allow taking lists and strings?
+    # job(
+    #     # cmd=, # TODO allow taking lists and strings?
 
-    )
+    # )
     pass
 
 
@@ -70,3 +126,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# TODO stuff I learnt:
+# TODO  systemd-analyze --user unit-paths 
+# TODO blame!
+#  systemd-analyze verify -- check syntax
