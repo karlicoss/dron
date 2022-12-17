04b403b7f8b1b0e339817ffb1c09a660a1d725a2
karlicoss <karlicoss@gmail.com>
2022-12-17 15:47:26 +0000

    move away systemd specific stuff into systemd.py

--- dron/common.py
+++ dron/common.py
@@ -1,6 +1,21 @@
-from pathlib import Path
-from typing import NamedTuple, Iterable, Tuple, Optional, Sequence, Union, Dict
+import argparse
 from dataclasses import dataclass
+from pathlib import Path
+import shlex
+import sys
+from typing import NamedTuple, Iterable, Tuple, Optional, Sequence, Union, Dict, Any
+
+
+# TODO can remove this? although might be useful for tests
+VERIFY_UNITS = True
+# TODO ugh. verify tries using already installed unit files so if they were bad, everything would fail
+# I guess could do two stages, i.e. units first, then timers
+# dunno, a bit less atomic though...
+
+class VerifyOff(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        global VERIFY_UNITS
+        VERIFY_UNITS = False
 
 
 class MonParams(NamedTuple):
@@ -62,3 +77,42 @@ Command = Union[PathIsh, Sequence[PathIsh]]
 OnCalendar = str
 TimerSpec = Dict[str, str] # meh # TODO why is it a dict???
 When = Union[OnCalendar, TimerSpec]
+
+
+MANAGED_MARKER = '(MANAGED BY DRON)'
+def is_managed(body: str) -> bool:
+    # switching off it because it's unfriendly to launchd
+    legacy_marker = '<MANAGED BY DRON>'
+    return MANAGED_MARKER in body or legacy_marker in body
+
+
+
+pytest_fixture: Any
+under_pytest = 'pytest' in sys.modules
+if under_pytest:
+    import pytest # type: ignore
+    pytest_fixture = pytest.fixture
+else:
+    pytest_fixture = lambda f: f # no-op otherwise to prevent pytest import
+
+
+Escaped = str
+def escape(command: Command) -> Escaped:
+    if isinstance(command, Escaped):
+        return command
+    elif isinstance(command, Path):
+        return escape([command])
+    else:
+        return ' '.join(shlex.quote(str(part)) for part in command)
+
+
+def wrap(script: PathIsh, command: Command) -> Escaped:
+    return shlex.quote(str(script)) + ' ' + escape(command)
+
+
+def test_wrap() -> None:
+    assert wrap('/bin/bash', ['-c', 'echo whatever']) == "/bin/bash -c 'echo whatever'"
+    bin = Path('/bin/bash')
+    assert wrap(bin, "-c 'echo whatever'") == "/bin/bash -c 'echo whatever'"
+    assert wrap(bin, ['echo', bin]) == "/bin/bash echo /bin/bash"
+    assert wrap('cat', bin) == "cat /bin/bash"
