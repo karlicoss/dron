dac48f15825471146239004208f30032bda08d96
karlicoss <karlicoss@gmail.com>
2023-06-04 22:59:14 +0100

    ci: seems that github actions can use systemd now, so get rid of circleci

---
+++ src/dron/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.fixture(scope='session', autouse=True)
+def disable_verify_units_if_no_systemd(request):
+    '''
+    If we can't use systemd, we need to suppress systemd-specific linting
+    '''
+    from .systemd import is_missing_systemd
+    from . import common
+
+    reason = is_missing_systemd()
+    if reason is not None:
+        common.VERIFY_UNITS = False
+    try:
+        yield
+    finally:
+        common.VERIFY_UNITS = True
