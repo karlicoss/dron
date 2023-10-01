24b5ca620f0c34f4b0e597635400f3859daf51e9
Dima Gerasimov <karlicoss@gmail.com>
2023-10-01 14:40:37 +0100

    launchd_wrapper: write output from notifications to the log as well

--- src/dron/notify/ntfy_common.py
+++ src/dron/notify/ntfy_common.py
@@ -5,9 +5,11 @@ uses https://github.com/dschep/ntfy
 import subprocess
 import logging
 import socket
+import sys
+from typing import NoReturn
 
 
-def run_ntfy(*, job: str, backend: str) -> None:
+def run_ntfy(*, job: str, backend: str) -> NoReturn:
     # TODO not sure what to do with --stdin arg here?
     # could probably use last N lines of log or something
     # TODO get last logs here?
@@ -17,4 +19,6 @@ def run_ntfy(*, job: str, backend: str) -> None:
         subprocess.check_call(['ntfy', '-b', backend, '-t', title, 'send', body])
     except Exception as e:
         logging.exception(e)
-        # TODO fallback on email
+        # TODO fallback on email?
+        sys.exit(1)
+    sys.exit(0)
