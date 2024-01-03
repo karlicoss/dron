fff25de58225a85c18c1b12345304cacf3811653
karlicoss <karlicoss@gmail.com>
2024-01-03 00:10:27 +0000

    telegram notification: send systemd log

--- src/dron/notify/telegram.py
+++ src/dron/notify/telegram.py
@@ -7,21 +7,26 @@ import logging
 import socket
 import sys
 
-from .common import get_parser
+from .common import get_parser, get_last_systemd_log, get_stdin
 
 
 def send(*, message: str) -> None:
     import telegram_send  # type: ignore[import-untyped]
+
     asyncio.run(telegram_send.send(messages=[message]))
+
 
 def main() -> None:
     p = get_parser()
     args = p.parse_args()
 
     job: str = args.job
+    stdin: bool = args.stdin
 
-    # TODO include some log?
     body = f'dron[{socket.gethostname()}]: {job} failed'
+
+    last_log = get_stdin() if stdin else get_last_systemd_log(job)
+    body += '\n' + '\n'.join(l.decode('utf8') for l in last_log)
 
     try:
         send(message=body)
@@ -30,6 +35,7 @@ def main() -> None:
         # TODO fallback on email?
         sys.exit(1)
     sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
