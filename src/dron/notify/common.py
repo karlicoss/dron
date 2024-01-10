ec95e4eae10f744acc180f003d864fc1ffd1ead9
karlicoss <karlicoss@gmail.com>
2024-01-10 02:42:08 +0000

    systemd notifications: fix systemctl timing out while retrieving logs for the last job run

--- src/dron/notify/common.py
+++ src/dron/notify/common.py
@@ -2,7 +2,7 @@
 import argparse
 import platform
 import shlex
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT, check_output
 import sys
 from typing import Iterator
 
@@ -22,9 +22,9 @@ def get_stdin() -> Iterator[bytes]:
 
 
 def get_last_systemd_log(job: str) -> Iterator[bytes]:
-    inf = '1000000'
-    cmd = ['systemctl', '--user', 'status', '--no-pager', '--lines', inf, job, '-o', 'cat']
-    yield ' '.join(map(shlex.quote, cmd)).encode('utf8') + b'\n\n'
+    # output unit status
+    cmd = ['systemctl', '--user', 'status', '--no-pager', job, '-o', 'cat']
+    yield b'$ ' + ' '.join(map(shlex.quote, cmd)).encode('utf8') + b'\n\n'
     with Popen(cmd, stdout=PIPE, stderr=STDOUT) as po:
         out = po.stdout
         assert out is not None
@@ -34,3 +34,22 @@ def get_last_systemd_log(job: str) -> Iterator[bytes]:
         0,
         3,  # 3 means failure due to job exit code
     }, rc
+
+    # for logs, we used to use --lines 1000000 in systemctl status
+    # however, from around 2024 it stated consuming too much time
+    # (as if it actually retrieved 1000000 lines and only then tooks the ones relevant to the unit??)
+
+    cmd = ['systemctl', '--user', 'show', job, '-p', 'InvocationID', '--value']
+    invocation_id = check_output(cmd, text=True)
+    invocation_id = invocation_id.strip()  # for some reason dumps multiple lines?
+    assert len(invocation_id) > 0  # just in case, todo maybe make defensive?
+
+    yield b'\n'
+    cmd = ['journalctl', '--no-pager', f'_SYSTEMD_INVOCATION_ID={invocation_id}']
+    yield b'$ ' + ' '.join(map(shlex.quote, cmd)).encode('utf8') + b'\n\n'
+    with Popen(cmd, stdout=PIPE, stderr=STDOUT) as po:
+        out = po.stdout
+        assert out is not None
+        yield from out
+    rc = po.poll()
+    assert rc == 0
